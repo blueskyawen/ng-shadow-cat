b5f7a886eff752722ece1b56b7ac88905e7554a9
blueskyawen <bluesky.liu@163.com>
2018-10-14 01:02:37 +0800

    extend toTop to document and window type

--- src/app/lib/nc-toTop/nc-toTop.component.ts
+++ src/app/lib/nc-toTop/nc-toTop.component.ts
@@ -10,25 +10,31 @@ import { Component,OnInit,Input,Output,EventEmitter,OnChanges,SimpleChanges } fr
 })
 export class NcToTopComponent implements OnInit {
   @Input() type : string = 'circle';
+  @Input() parentId : string;
   typeClasses : any = {};
   timer : any = null;
   displayType : string;
-  window = window;
+  contentDom : any;
 
   constructor() {
   }
 
   ngOnInit() {
+    this.contentDom = this.parentId ?
+        (this.parentId !== 'window' ? document.getElementById(this.parentId) : window) : window;
     this.displayType = this.type === 'circle' ? 'flex' : 'block';
     this.typeClasses = {'nc-backTop-circle': this.type === 'circle','nc-backTop-rocket': this.type === 'rocket'};
-    this.window.onscroll = () => {
-      var backtop = this.window.pageYOffset;
-      if(backtop > 0){
-        this.setBackTopPlace(this.displayType);
-      } else {
-        this.setBackTopPlace("none");
-      }
-    };
+    if(this.contentDom) {
+      this.contentDom.onscroll = () => {
+        let backtop = (!this.parentId || this.parentId === 'window') ?
+            this.contentDom.pageYOffset : this.contentDom.scrollTop;
+        if(backtop > 0) {
+          this.setBackTopPlace(this.displayType);
+        } else {
+          this.setBackTopPlace("none");
+        }
+      };
+    }
   }
 
   setBackTopPlace(type : any) {
@@ -38,12 +44,15 @@ export class NcToTopComponent implements OnInit {
   }
 
   scrollWindow(){
-    this.timer = setInterval(() => {
-      var backtop = this.window.pageYOffset;
-      this.window.scrollBy(0,-60);
-      if(backtop < 1){
-        clearInterval(this.timer);
-      }
-    },100);
+    if(this.contentDom) {
+      this.timer = setInterval(() => {
+        let backtop = (!this.parentId || this.parentId === 'window') ?
+            this.contentDom.pageYOffset : this.contentDom.scrollTop;
+        this.contentDom.scrollBy(0,-60);
+        if(backtop < 1){
+          clearInterval(this.timer);
+        }
+      },100);
+    }
   }
 }
