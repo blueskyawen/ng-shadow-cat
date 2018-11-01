0f840f360342c0df94167f2d69a5cbe7cba86270
blueskyawen <bluesky.liu@163.com>
2018-11-02 00:03:13 +0800

    add npm bao

---
+++ index.js
@@ -0,0 +1,4 @@
+/**
+ * Created by liuxuwen on 18-11-1.
+ */
+export * from './src/app/lib/lib.module';
---
+++ src/app/lib/index.d.ts
@@ -0,0 +1,4 @@
+/**
+ * Created by liuxuwen on 18-11-1.
+ */
+export {LibModule} from './lib.module';
