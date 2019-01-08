3e01db0d9f4c35cc0f0483dcf9d5d57583206981
blueskyawen <bluesky.liu@163.com>
2019-01-08 11:21:49 +0800

    up nc-month-selector.component.ts

--- src/app/lib/nc-date-time-selector/nc-month-selector.component.ts
+++ src/app/lib/nc-date-time-selector/nc-month-selector.component.ts
@@ -1,11 +1,12 @@
 import { Component,OnInit,Input,Output,EventEmitter,ViewChild,ElementRef,AfterViewInit } from '@angular/core';
+import { NcDateSelectorBase } from './nc-date-selector.base';
 
 @Component({
     selector: 'nc-month-selector',
     templateUrl: './nc-month-selector.component.html',
     styleUrls: ['./nc-month-selector.component.css','./nc-date-time-selector.component.css']
 })
-export class NcMonthSelectorComponent implements OnInit {
+export class NcMonthSelectorComponent extends NcDateSelectorBase implements OnInit {
     @Input() type : string = 'single';
     @Input() date : any = {};
     @Output() dateChange = new EventEmitter();
@@ -17,34 +18,18 @@ export class NcMonthSelectorComponent implements OnInit {
     months : any[] = [{value:0,label:'1月'},{value:1,label:'2月'},{value:2,label:'3月'},{value:3,label:'4月'},
         {value:4,label:'5月'},{value:5,label:'6月'},{value:6,label:'7月'},{value:7,label:'8月'},
         {value:8,label:'9月'},{value:9,label:'10月'},{value:10,label:'11月'},{value:11,label:'12月'}];
-    value : string;
-    selectorStyle : any = {};
-    isHiddenSelector : boolean = true;
-    isOverSelector : boolean = false;
     curYear : any;
-    formatLabel : string;
 
     constructor() {
+        super();
     }
 
     ngOnInit() {
+        this.selectorType = this.type;
         this.listenDocuClick();
         this.getFormat();
         this.setStyleAndClass();
         this.initData();
-    }
-
-    listenDocuClick() {
-        if(this.type == 'input') {
-            document.addEventListener('click', (event) => {
-                if (!this.isOverSelector) {
-                    this.isHiddenSelector = true;
-                    //this.recoverData();
-                    //this.setDateValue();
-                }
-                event.stopPropagation();
-            });
-        }
     }
 
     getFormat() {
@@ -71,10 +56,6 @@ export class NcMonthSelectorComponent implements OnInit {
         }
     }
 
-    formatValue(value : number) {
-        return value < 10 ? '0'+value : value.toString();
-    }
-
     prevPage() {
         this.curYear--;
     }
@@ -92,21 +73,13 @@ export class NcMonthSelectorComponent implements OnInit {
             this.closeSelector();
             return;
         }
-        this.clearItems();
+        this.clearItems(this.months);
         item.active = true;
         this.date.setFullYear(this.curYear);
         this.date.setMonth(item.value);
         this.setDateValue();
         this.dateChange.emit(this.date);
         this.closeSelector();
-    }
-
-    clearItems() {
-        this.months.forEach((item) => {
-            if(!item.disable) {
-                item.active = false;
-            }
-        });
     }
 
     openSelector() {
@@ -116,26 +89,8 @@ export class NcMonthSelectorComponent implements OnInit {
         }
     }
 
-    closeSelector() {
-        if(this.type === 'input') {
-            this.isHiddenSelector = true;
-        }
-    }
-
     recoverData() {
         this.curYear = this.date.getFullYear();
-    }
-
-    mouseover() {
-        if(this.type === 'input') {
-            this.isOverSelector = true;
-        }
-    }
-
-    mouseout() {
-        if(this.type === 'input') {
-            this.isOverSelector = false;
-        }
     }
 }
 
